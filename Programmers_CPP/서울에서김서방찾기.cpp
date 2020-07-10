61a6c0fe382bf8520bab8eca5dbbf7f53380d011
SeryeongChoi <sr5757@naver.com>
2020-07-10 20:38:13 +0900

    20200710

---
+++ Programmers_CPP/서울에서김서방찾기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+	string answer = "";
+	int a = 0;
+	for (int i = 0; i < seoul.size(); i++) {
+		a = seoul[i].find("Kim");
+		if (a > -1) {
+			a = i;
+			break;
+		}
+	}
+	answer += "김서방은 ";
+	answer += to_string(a);
+	answer += "에 있다";
+
+	return answer;
+}
